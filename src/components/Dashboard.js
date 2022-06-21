93a4febc2c9b195cbde4e782236a2f10d4aea3d3
charu.chachra <charu.chachra@juspay.in>
2022-06-21 18:53:48 +0530

    Logout button added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 //rafce
-import React, { useEffect } from 'react'
+import React, {useEffect, useState } from 'react'
+// import React, { useState } from 'react'
 import { Grid } from "@material-ui/core";
 import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 import Details from './Details/Details'
@@ -9,6 +10,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     let navigate = useNavigate()
+    const [authToken, setAuthToken] = useState(false)
+   
     useEffect(()=>{
         let authToken = sessionStorage.getItem('Auth Token')
 
@@ -19,11 +22,17 @@ const Dashboard = () => {
         if (!authToken) {
             navigate('/login')
         }
-    },[])
-
+    },[authToken])
+let onclick=()=>
+{
+    sessionStorage.setItem('Auth Token', "")
+    setAuthToken(!authToken)
+  
+}
     const classes = useStyles();
     return (
         <div style ={{"background": "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(https://raw.githubusercontent.com/adrianhajdin/speechly_expense_tracker_project/main/src/assets/money.png)"}}>
+            <button onClick={onclick} style={{display:'flex', alignItems:'right'}}>Logout</button>
             <Grid className={classes.grid} container spacing={0} alignItems="center" style={{ height: '100vh' }}>
                 <Grid item xs={12} sm={3} className={classes.mobile}>
                     <Details title="Income" />
